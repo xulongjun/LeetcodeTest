fbb1e0959bd4cf84208dbb61a85187715a120e5d
LongjunXU <xulongjun.fr@gmail.com>
2020-06-23 22:52:11 +0200

    Add 222

---
+++ Algo/BinaryTree/222_CountCompleteTreeNodes.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LeetCodeTest.Algo.BinaryTree
+{
+    public class Solution222
+    {
+        public int CountNodes(TreeNode root)
+        {
+            return root == null ? 0 : CountNodes(root.left) + CountNodes(root.right) + 1;
+        }
+    }
+}
+
